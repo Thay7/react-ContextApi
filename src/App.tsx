74dead157f83b8e2c6511106d75974a8697aff5d
Thay7 <thaylynnefranca@gmail.com>
2023-02-03 12:00:35 -0300

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {  BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useContext } from 'react'
 
-function App() {
+import { SignUp } from './pages/SignUp'
+import { ShowData } from './pages/ShowData'
+import { Context } from './contexts/Context'
+
+
+const App = () => {
+  const { state, dispatch } = useContext(Context)
+
+  const handleSwitchTheme = () => {
+    if(state.theme.status === 'light') {
+      dispatch({
+        type: 'CHANGE_STATUS',
+        payload: {
+          status: 'dark'
+        }
+      }) 
+    } else {
+      dispatch({
+        type: 'CHANGE_STATUS',
+        payload: {
+          status: 'light'
+        }
+      })
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <div 
+        style={{backgroundColor: state.theme.status === 'light' ? '#FFF' : '#000',
+        color: state.theme.status === 'light' ? '#000' : '#FFF' 
+      }}>
+            <h1>Titulo da Página</h1>
+            Tema: {state.theme.status}
+            <button onClick={handleSwitchTheme}>Switch Theme</button>
+            <hr />
+            <Routes>
+              <Route path='/' element={<SignUp/>}/>
+              <Route path='/exibir' element={<ShowData/>}/>
+            </Routes>
+      </div>
+    </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,57 @@
+import { stat } from 'fs'
+import { useContext, useState } from 'react'
+import { Link } from 'react-router-dom'
+
+import { Context } from '../contexts/Context'
+
+export const SignUp = () => {
+    const { state, dispatch } = useContext(Context)
+    const [nameInput, setNameInput] = useState(state.user.name)
+    const [ageInput, setAgeInput] = useState(state.user.age)
+
+    const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNameInput(e.target.value)
+    }
+
+    const handleAgeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setAgeInput(parseInt(e.target.value))
+    }
+
+    const handleSave = () => {
+        dispatch({
+            type: 'CHANGE_NAME',
+            payload: {
+                name: nameInput
+            }
+        })
+        dispatch({
+            type: 'CHANGE_AGE',
+            payload: {
+                age: ageInput
+            }
+        })
+    }
+ 
+    return (
+        <div>
+            <h3>Tela SignUp (Tema: {state.theme.status})</h3>
+            <input 
+                type="text"
+                placeholder='Digite um nome'
+                value={nameInput}
+                onChange={handleNameChange}
+            />
+            <input
+                type="text"
+                placeholder='Digite sua idade'
+                value={ageInput}
+                onChange={handleAgeChange}
+            />
+
+            <button onClick={handleSave}>Salvar</button>
+
+            <br />
+            <Link to='/exibir'>Ir para a tela ShowData</Link>
+        </div>
+        )
+}
